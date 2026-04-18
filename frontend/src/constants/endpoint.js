719110a19b66b5e447ce19f5854c298d7d3aa1ed
DASH-2 <paradox7955@gmail.com>
2026-04-18 16:50:12 +0900

    API_STAGE 값 초기화
    CodeBuild 환경변수에 이미 /dev 경로가 명시되어 있음.
    변경 전으로 시도 시 환경변수 뒤에 /dev가 한번 더 붙음.

--- frontend/src/constants/endpoint.js
+++ frontend/src/constants/endpoint.js
@@ -1,5 +1,5 @@
 const API_HOST = process.env.REACT_APP_API_HOST || "http://localhost:4000";
-const API_STAGE = "/dev";
+const API_STAGE = "";
 
 export const API_BASE_URL = `${API_HOST}${API_STAGE}`;
 
