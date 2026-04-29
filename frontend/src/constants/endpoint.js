85059b19e6c438cf80ffc7fb6128ba2e535888e9
DASH-2 <paradox7955@gmail.com>
2026-04-29 16:47:51 +0900

    fix: Websocket 환경변수명 오타 수정
    - process.env.REACT_APP_WS_URL -> process.env.REACT_APP_WS_HOST 로 수정

--- frontend/src/constants/endpoint.js
+++ frontend/src/constants/endpoint.js
@@ -2,7 +2,7 @@ const API_HOST = process.env.REACT_APP_API_HOST || "http://localhost:4000/dev";
 
 export const API_BASE_URL = API_HOST;
 export const API_WS_URL =
-  process.env.REACT_APP_WS_URL || "ws://localhost:4001";
+  process.env.REACT_APP_WS_HOST || "ws://localhost:4001";
 
 export const ENDPOINTS = {
   auth: {
